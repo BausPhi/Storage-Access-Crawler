26e43b0fd8fa17d277022b84a29aa20c957ec41e
BausPhi <philipp.baus.studium@googlemail.com>
2024-06-26 14:52:50 +0200

    🐛 Fixed restart script to account for new container names due to directory name change

--- StorageAccessCrawler/src/restart.sh
+++ StorageAccessCrawler/src/restart.sh
@@ -1,4 +1,4 @@
-docker container rm pythoncrawler_db_1 -f
-docker container rm pythoncrawler_pycrawler_1 -f
+docker container rm storageaccesscrawler_db_1 -f
+docker container rm storageaccesscrawler_pycrawler_1 -f
 docker container prune -f
 docker-compose up -d --build
