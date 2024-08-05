efb9d05f7c625711d0b10a604cdacf5d80dc9ae3
BausPhi <philipp.baus.studium@googlemail.com>
2024-08-05 19:14:17 +0200

    🐛 Wrong browser object used

--- StorageAccessCrawler/src/modules/storageaccessapi.py
+++ StorageAccessCrawler/src/modules/storageaccessapi.py
@@ -361,7 +361,7 @@ class StorageAccessApi(Module):
             """
             if self.saa_found:
                 store_site_data_db(self.top_level, logger=self.crawler.log, site=self.crawler.task.site,
-                                   browser=self.crawler.browser.name)
+                                   browser=Config.BROWSER)
             self.top_level = FrameHierarchy(url="", sha1="undefined", content=b"undefined", saa=False)
             self.saa_found = False
 
