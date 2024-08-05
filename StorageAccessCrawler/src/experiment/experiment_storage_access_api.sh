eeaf5df4807f0c83ab819daf73e19117ab17280a
BausPhi <philipp.baus.studium@googlemail.com>
2024-08-05 17:05:01 +0200

    🔧 Changed number of concurrent crawlers to 30

--- StorageAccessCrawler/src/experiment/experiment_storage_access_api.sh
+++ StorageAccessCrawler/src/experiment/experiment_storage_access_api.sh
@@ -27,7 +27,7 @@ echo "Experiment storageaccessapi starting"
 echo "This might take a while. You can watch the experiment via VNC or the logs in /pycrawler/logs"
 
 # now launch the actual experiment
-python3 main.py -m StorageAccessApi -j storageaccessapi -c 10
+python3 main.py -m StorageAccessApi -j storageaccessapi -c 30
 
 echo "Experiment storageaccessapi completed"
 echo "You can inspect the raw results in the database"
