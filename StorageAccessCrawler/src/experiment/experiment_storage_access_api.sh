73b2b7efddf2d3fa76ee7843227d22f31514c026
BausPhi <philipp.baus.studium@googlemail.com>
2024-10-16 08:47:42 +0200

    🔧 Change number of crawlers

--- StorageAccessCrawler/src/experiment/experiment_storage_access_api.sh
+++ StorageAccessCrawler/src/experiment/experiment_storage_access_api.sh
@@ -26,7 +26,7 @@ echo "Experiment storageaccessapi starting"
 echo "This might take a while. You can watch the experiment via VNC or the logs in /pycrawler/logs"
 
 # now launch the actual experiment
-python3 main.py -m StorageAccessApi -j storageaccessapi -c 30
+python3 main.py -m StorageAccessApi -j storageaccessapi -c 20
 
 echo "Experiment storageaccessapi completed"
 echo "You can inspect the raw results in the database"
